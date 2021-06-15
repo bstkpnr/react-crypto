d4f5a8e3de5d47cdebd2b791cca69549c18ac655
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-06-15 15:53:23 +0300

    style card

--- src/component/Coin.js
+++ src/component/Coin.js
@@ -10,33 +10,29 @@ function Coin(){
     return(
         <div className="container text-center">
         <div className="bg-white m-1 p-2 rounded">
-          <h1 className="text-center text-success">Cryptos Mryptos</h1>
+          <h1 className="text-center text-dark">Cryptos </h1>
         </div>
   
-        <div className="row offset-4">
-          <div className="col-6 mt-5 text-white ">
+        <div className="row offset-4" >
+          <div className="col-6 mt-5 text-white " >
             {filter.map((item) => (
-              <div className="card bg-success shadow" key={item.id}>
+              <div className="card bg-dark  shadow" key={item.id} >
                 <div
                   className="mx-auto"
-                  style={{ width: "244px", height: "244px" }}
+                  style={{ width: "244px", height: "244px"}}
                 >
                   <img src={item.image} className="card-img-top" alt={item.id} />
                 </div>
-                <div className="card-body">
-                  <h5 className="card-title">
+                <div className="card-body" style={{backgroundColor:'#FDFAF6'}}>
+                  <h5 className="card-title" style={{color:'#325288'}}>
                     Current Price : {item.current_price}
                   </h5>
-                  <p className="card-text">Name : {item.name}</p>
-                  <p className="card-text">Rank : {item.market_cap_rank}</p>
-                  <p className="card-text">Total : {item.total_volume}</p>
-                  {/* <p className="card-text">Last Updated : {item.last_updated}</p>
-                  <p className="card-text">
-                    Price Change Percentage - 24h :{" "}
-                    {item.price_change_percentage_24h}
-                  </p> */}
+                  <p className="card-text" style={{color:'#325288'}}>Name : {item.name}</p>
+                  <p className="card-text" style={{color:'#325288'}}>Rank : {item.market_cap_rank}</p>
+                  <p className="card-text" style={{color:'#325288'}}>Total : {item.total_volume}</p>
+                
                 </div>
-                <Link to="/" className="  btn btn-warning shadow">
+                <Link to="/" className="btn btn-secondary">
                   Home
                 </Link>
               </div>
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,8 @@
 import React, { useContext } from "react";
 import { CoinContext } from "../context/CoinContext";
 import { Link } from "react-router-dom";
-import {InputGroup,FormControl} from 'react-bootstrap'
-import {FaSearchDollar} from 'react-icons/fa'
-
+import { InputGroup, FormControl,Card } from "react-bootstrap";
+import { FaSearchDollar } from "react-icons/fa";
 
 function Home({ search, coinName }) {
   const context = useContext(CoinContext);
@@ -11,24 +10,13 @@ function Home({ search, coinName }) {
   return (
     <div className="container mx-auto px-4">
       <div className="bg-white m-1 p-2 rounded">
-        <h1 className="text-center text-success">CRYTPO</h1>
+        <h1 className="text-center text-dark">CRYTPO</h1>
       </div>
       <div className="row">
-        {/* <form>
-          <div className="col-md-5 mx-auto mt-4">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Search..."
-              value={search}
-              onChange={coinName}
-            />
-          </div>
-        </form> */}
         <InputGroup className="mb-3">
           <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroup-sizing-default" >
-           < FaSearchDollar />
+            <InputGroup.Text id="inputGroup-sizing-default">
+              <FaSearchDollar />
             </InputGroup.Text>
           </InputGroup.Prepend>
           <FormControl
@@ -37,12 +25,10 @@ function Home({ search, coinName }) {
             placeholder="Search Crypto"
             value={search}
             onChange={coinName}
-            
-            
           />
         </InputGroup>
       </div>
-      <div className="row">
+      <div className="row md-3">
         {context.coin
           .filter((val) => {
             if (search === "") {
@@ -53,35 +39,29 @@ function Home({ search, coinName }) {
             return false;
           })
           .map((val) => (
-            <div className="col-6 col-md-4 mt-5" key={val.id}>
-              <div className="card text-center bg-success shadow">
-                <div
-                  className="mx-auto w-50 mt-5 mb-5 "
-                  style={{ width: "244px", height: "150px" }}
-                >
-                  <img src={val.image} className="card-img-top" alt={val.id} />
-                </div>
-                <div className="card-body text-white">
-                  <h5 className="card-title">
-                    Current Price : {val.current_price}
-                  </h5>
-                  <p className="card-text">Name : {val.name}</p>
-                  <p className="card-text">Rank : {val.market_cap_rank}</p>
-                  <p className="card-text">Total : {val.total_volume}</p>
-                  {/* <p className="card-text">Last Updated : {val.last_updated}</p>
-                  <p className="card-text">
-                    Price Change Percentage - 24h :{" "}
-                    {val.price_change_percentage_24h}
-                  </p> */}
-                  <Link to={val.id} className="btn btn-warning shadow">
-                    Details
-                  </Link>
-                </div>
-              </div>
-            </div>
+            <Card style={{width:"19rem",margin:"20px"}} className="card border-danger mb-3">
+              <Card.Img variant="top" src={val.image} />
+              <Card.Body style={{backgroundColor:'#BBBBBB'}}>
+                <Card.Text>Name:{val.name}</Card.Text>
+                <Card.Text>Rank : {val.market_cap_rank}</Card.Text>
+                <Card.Text>Total : {val.total_volume}</Card.Text>
+                <Card.Text>Last Updated : {val.last_updated}</Card.Text>
+                <Card.Text>
+                  Price Change Percentage - 24h :{" "}
+                  {val.price_change_percentage_24h}
+                </Card.Text>
+                <Link to={val.id} className="btn btn-dark">
+                  Details
+                </Link>
+              </Card.Body>
+            </Card>
           ))}
+        
+        //{" "}
       </div>
+      //{" "}
     </div>
+    // </div>
   );
 }
 export default Home;
